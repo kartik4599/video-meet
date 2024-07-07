231d83b54e37ca768dc8b2a8e116e8a3046323a4
dharmik soni <dharmiksoni833@gmail.com>
2024-07-07 16:51:30 +0530

    Added meeting page and getcall hook

---
+++ app/(root)/meeting/[id]/page.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useUser } from "@clerk/nextjs";
+import React, { useState } from "react";
+import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
+import MeetingSetup from "@/components/MeetingSetup";
+import { useGetCallById } from "@/hooks/useGetCallById";
+import Loader from "@/components/Loader";
+
+const page = ({ params }: { params: { id: string } }) => {
+  const { user, isLoaded } = useUser();
+  const [isSetupCompleted, setisSetupCompleted] = useState(false);
+
+  const { call, isCallLoading } = useGetCallById(params.id);
+
+  if (isCallLoading || !isLoaded) return <Loader />;
+
+  return (
+    <main className="h-screen w-full">
+      <StreamCall>
+        <StreamTheme>
+          {isSetupCompleted ? <MeetingSetup /> : <MeetingSetup />}
+        </StreamTheme>
+      </StreamCall>
+    </main>
+  );
+};
+
+export default page;
